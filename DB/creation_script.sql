cb1ceaf63e6b082b54eb8540ccbf64f054bfb669
secured128 <secured128@gmail.com>
2019-02-18 21:50:11 +0200

    DB

--- DB/creation_script.sql
+++ DB/creation_script.sql
@@ -23,3 +23,27 @@ ALTER TABLE users
 
 CREATE UNIQUE INDEX user_unique_idx
     ON users USING btree(name);
+
+
+-- Table: public.suppliers
+
+-- DROP TABLE public.suppliers;
+
+CREATE TABLE public.suppliers
+(
+  id bigint NOT NULL,
+  name character varying(255),
+  address character varying(255),
+  city character varying(255),
+  telephone_num character varying(255),
+  fax character varying(255),
+  email character varying(255),
+  company_no character varying(255),
+  CONSTRAINT suppliers_pkey PRIMARY KEY (id),
+  CONSTRAINT suppliers_unique_idx UNIQUE (name)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.suppliers
+  OWNER TO food_manager;
